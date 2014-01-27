af6a84aa39f7aa9f823fd7b11c4e1bc77547ab9b
TomyLobo <tomylobo@nurfuerspam.de>
2014-01-27 08:48:17 +0100

    Reordered some methods and added basic javadocs.

--- src/main/java/org/bukkit/util/AffineTransform.java
+++ src/main/java/org/bukkit/util/AffineTransform.java
@@ -12,40 +12,130 @@ public class AffineTransform {
         this.offset = offset;
     }
 
+    /**
+     * Constructs and affine transformation that does nothing.
+     */
     public AffineTransform() {
         this(Matrix3.identity(), new Vector());
     }
 
+    /**
+     * Constructs an affine transformation that rotates the specified number of degrees around the specified axis and then shifts by the specified offset.
+     *
+     * @param offset
+     * @param axis
+     * @param radians
+     * @return
+     */
     public static AffineTransform fromOffsetAxisAndAngle(Vector offset, Vector axis, double radians) {
         return new AffineTransform(Matrix3.fromAxisAndAngle(axis, radians), offset.clone());
     }
 
+    /**
+     * Constructs an affine transformation from the specified offset and angles.
+     * <p>
+     * The following conditions apply:
+     * <ul>
+     * <li>X axis points to the left
+     * <li>Y axis points upward
+     * <li>Z axis points forward
+     * </ul>
+     *
+     * @param offset
+     * @param yaw
+     * @param pitch
+     * @param roll
+     * @return
+     */
     public static AffineTransform fromOffsetAndAngles(Vector offset, double yaw, double pitch, double roll) {
         return new AffineTransform(Matrix3.fromAngles(yaw, pitch, roll), offset.clone());
     }
 
+    /**
+     * Constructs an affine transformation from the specified {@link Location}.
+     * <p>
+     * The following conditions apply:
+     * <ul>
+     * <li>X axis points to the left
+     * <li>Y axis points upward
+     * <li>Z axis points forward
+     * </ul>
+     *
+     * @param location
+     * @return
+     */
     public static AffineTransform fromOffsetAndAngles(Location location) {
         return new AffineTransform(Matrix3.fromAngles(location), location.toVector());
     }
 
+
+    /**
+     * Transforms an axis from the local reference frame described by this
+     * affine transformation into the world reference frame.
+     *
+     * @param axis a local-coordinate axis
+     * @return a world-coordinate axis
+     */
+    public Vector toWorldAxis(Vector axis) {
+        return orthogonalMatrix.multiply(axis);
+    }
+
+    /**
+     * Transforms an axis from the world reference frame into the local
+     * reference frame described by this affine transformation.
+     * <p>
+     * The following conditions apply:
+     * <ul>
+     * <li>X axis points to the left
+     * <li>Y axis points upward
+     * <li>Z axis points forward
+     * </ul>
+     *
+     * @param axis a world-coordinate axis
+     * @return a local-coordinate axis
+     */
+    public Vector toLocalAxis(Vector axis) {
+        return orthogonalMatrix.multiplyTranspose(axis);
+    }
+
+    /**
+     * Transforms a position from the local reference frame described by this
+     * affine transformation into the world reference frame.
+     * <p>
+     * The following conditions apply:
+     * <ul>
+     * <li>X axis points to the left
+     * <li>Y axis points upward
+     * <li>Z axis points forward
+     * </ul>
+     *
+     * @param position a local-coordinate position
+     * @return a world-coordinate position
+     */
     public Vector toWorld(Vector position) {
         Validate.notNull(position);
 
         return toWorldAxis(position).add(offset);
     }
 
-    public Vector toWorldAxis(Vector axis) {
-        return orthogonalMatrix.multiply(axis);
-    }
-
+    /**
+     * Transforms a position from the world reference frame into the local
+     * reference frame described by this affine transformation.
+     * <p>
+     * The following conditions apply:
+     * <ul>
+     * <li>X axis points to the left
+     * <li>Y axis points upward
+     * <li>Z axis points forward
+     * </ul>
+     *
+     * @param position a world-coordinate position
+     * @return a local-coordinate position
+     */
     public Vector toLocal(Vector position) {
         Validate.notNull(position);
 
         return toLocalAxis(position.clone().subtract(offset));
-    }
-
-    public Vector toLocalAxis(Vector axis) {
-        return orthogonalMatrix.multiplyTranspose(axis);
     }
 
 
